cdfb50dcb7483cdaf5e13dd609ad05be7be09543
Caddy Dz <tbitw31@gmail.com>
2018-01-01 17:08:52 +0100

    Iterate through args
    1- For Loop
    2- Main Attributes
    3- Compiling
    
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    printf("Hello, World!\n");
+    for (int i = 0; i < argc; i++) {
+        printf("arg %d : %s\n", i, argv[i]);
+    }
     return 0;
 }
