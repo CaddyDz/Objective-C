2b005f19ff0aa6ce840aab77fe65284abfbda25c
Caddy Dz <tbitw31@gmail.com>
2018-01-01 16:36:42 +0100

    Hello, World!
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -1,15 +1,6 @@
-//
-//  main.c
-//  ObjCTut
-//
-//  Created by CGP Grey on 1/1/18.
-//  Copyright © 2018 Salim Djerbouh. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     printf("Hello, World!\n");
     return 0;
 }
