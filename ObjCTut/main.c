7e8de9d1d20a6257214683baa08cf5a4e47e15d9
Caddy Dz <tbitw31@gmail.com>
2018-01-01 17:33:07 +0100

    Data types and usage in printf
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -4,5 +4,15 @@ int main(int argc, const char * argv[]) {
     for (int i = 0; i < argc; i++) {
         printf("arg %d : %s\n", i, argv[i]);
     }
+    /*
+     short %d
+     int %d
+     long %ld
+     float %f
+     double %lf
+     char %c
+     struct => miscellaneous data type
+     pointer => location of data in memory
+    */
     return 0;
 }
