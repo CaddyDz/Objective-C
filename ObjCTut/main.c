acdc2ff95f3943944d514c35176233390b7e3bbe
Caddy Dz <tbitw31@gmail.com>
2018-01-01 16:27:50 +0100

    Initial Commit

---
+++ ObjCTut/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ObjCTut
+//
+//  Created by CGP Grey on 1/1/18.
+//  Copyright © 2018 Salim Djerbouh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
