6ee4afc81cbb117c05e858a1a61e44f4c9cfdd33
Caddy Dz <tbitw31@gmail.com>
2018-01-05 17:23:26 +0100

    Scanf / User Input
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    for (int i = 0; i < argc; i++) {
-        printf("arg %d : %s\n", i, argv[i]);
-    }
-    /*
-     short %d
-     int %d
-     long %ld
-     float %f
-     double %lf
-     char %c
-     struct => miscellaneous data type
-     pointer => location of data in memory
-    */
+    float fTemp;
+    printf("Enter Temp in F : ");
+    scanf("%f", &fTemp);
+    float cTemp = (fTemp - 32) / 1.8;
+    printf("%.1f degree Celsius\n", cTemp);
     return 0;
 }
