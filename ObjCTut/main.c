911a1dba2e9ab5cd76172375524cacb5a8a69653
Caddy Dz <tbitw31@gmail.com>
2018-01-06 16:15:27 +0100

    Comparison Operators
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -13,5 +13,12 @@ int main(int argc, const char * argv[]) {
     } else {
         printf("You're in High School\n");
     }
+    
+    // Logical Operators : && || !
+    
+    if ((age >= 12) || (age <= 13)) {
+        printf("You're in Elementary School\n");
+    }
+    
     return 0;
 }
