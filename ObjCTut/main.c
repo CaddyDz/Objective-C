fd5a81f5dfee935dd9f7f81f6e0352c0d176bcea
Caddy Dz <tbitw31@gmail.com>
2018-01-06 16:09:25 +0100

    If Else Statement
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -4,23 +4,14 @@
 
 int main(int argc, const char * argv[]) {
     
-    int bigInt = 2147483647;
+    int age = 13;
     
-    printf("Big Int : %d\n", bigInt + 1);
-    
-    long bigLong = 9223372036854775807;
-    
-    printf("Big Long : %ld\n", bigLong + 1);
-    
-    printf("Min Float : %e\n", FLT_MIN);
-    
-    printf("Max Float : %e\n", FLT_MAX);
-    
-    float pi = 3.1415926;
-    
-    printf("3.1415926 + .0000001 = %.7f\n", pi + .0000001);
-    
-    printf("Precise Decimal Digits : %d\n", FLT_DIG);
-    
+    if (age <= 6) {
+        printf("You're in Kindergarten\n");
+    } else if (age <= 13) {
+        printf("You're in Elementary\n");
+    } else {
+        printf("You're in High School\n");
+    }
     return 0;
 }
