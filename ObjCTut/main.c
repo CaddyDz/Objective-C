b65a8211166c505dcfbf6a429ee82981e06ffa6a
Caddy Dz <tbitw31@gmail.com>
2018-01-06 16:49:12 +0100

    Math
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -5,20 +5,45 @@
 
 int main(int argc, const char * argv[]) {
     
-    int age = 13;
+    // --------- Math -----------
     
-    if (age <= 6) {
-        printf("You're in Kindergarten\n");
-    } else if (age <= 13) {
-        printf("You're in Elementary\n");
-    } else {
-        printf("You're in High School\n");
-    }
+    printf("3 + 2 = %d\n", 3 + 2);
+    printf("3 - 2 = %d\n", 3 - 2);
+    printf("3 * 2 = %d\n", 3 * 2);
+    printf("3 / 2 = %d\n", 3 / 2);
+    printf("3 %% 2 = %d\n", 3 % 2);
     
-    // The Boolean type : true = 1 & false = 0
-    bool isElementary = ((age >= 12) && (age <= 13));
+    // Use cast to get decimals
+    printf("3 / 2 = %.2f\n", (float) 3 / 2);
     
-    printf("Is in elementary : %d\n", isElementary);
+    // Order of operations
+    printf("2 + 1 * 3 = %d\n", 2 + 1 * 3);
+    printf("(2 + 1) * 3 = %d\n", (2 + 1) * 3);
+    
+    // Shorthand notations (++ or --)
+    int i = 0;
+    
+    printf("i++ = %d\n", i++);
+    printf("++i = %d\n", ++i);
+    
+    // You can also use -= *= /= %=
+    i = 0;
+    printf("i += 5 = %d\n", i += 5);
+    
+    // Many math functions
+    // ceil(double), floor(double), fabs(double)
+    // sqrt(double), exp(double, exp2(double)
+    // log(double), log2(double), pow(double, double)
+    
+    printf("Ceiling 5.1 = %lf\n", ceil(5.1));
+    printf("Floor 5.9 = %lf\n", floor(5.9));
+    printf("Absolute value of 5.9 = %lf\n", fabs(5.9));
+    printf("Square root of 16 = %lf\n", sqrt(16));
+    printf("Exponent of 2 = %lf\n", exp(2));
+    printf("Exp2 of 4 = %lf\n", exp2(4));
+    printf("log of 1024 = %lf\n", log(1024));
+    printf("log2 of 1024 = %lf\n", log2(1024));
+    printf("Power of 9 in 2 = %lf\n", pow(9, 2));
     
     return 0;
 }
