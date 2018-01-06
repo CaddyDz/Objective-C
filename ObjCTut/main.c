30e41387ab416fd7a8f673f87b131d6ecf32e4ac
Caddy Dz <tbitw31@gmail.com>
2018-01-06 16:05:22 +0100

    Data Type Precision
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <math.h>
+#include <float.h>
 
 int main(int argc, const char * argv[]) {
-    float fTemp;
-    printf("Enter Temp in F : ");
-    scanf("%f", &fTemp);
-    float cTemp = (fTemp - 32) / 1.8;
-    printf("%.1f degree Celsius\n", cTemp);
+    
+    int bigInt = 2147483647;
+    
+    printf("Big Int : %d\n", bigInt + 1);
+    
+    long bigLong = 9223372036854775807;
+    
+    printf("Big Long : %ld\n", bigLong + 1);
+    
+    printf("Min Float : %e\n", FLT_MIN);
+    
+    printf("Max Float : %e\n", FLT_MAX);
+    
+    float pi = 3.1415926;
+    
+    printf("3.1415926 + .0000001 = %.7f\n", pi + .0000001);
+    
+    printf("Precise Decimal Digits : %d\n", FLT_DIG);
+    
     return 0;
 }
