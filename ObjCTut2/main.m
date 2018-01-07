d48dc557b1cb799dffd777d23ce258991271df55
Caddy Dz <tbitw31@gmail.com>
2018-01-07 11:41:17 +0100

    Auto Release Oiil
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/main.m
+++ ObjCTut2/main.m
@@ -1,7 +1,8 @@
-// The Foundation framework consists of fundamental classes used to develop Objective C programs
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
+    
+    // The ARC (Automatic Reference Counting) signals for the destruction of objects when they are no longer needed
     @autoreleasepool {
         NSLog(@"Hello, World!");
     }
