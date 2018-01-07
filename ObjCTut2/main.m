dd15b82d09f0d2f080523c8830729ab335a8ed02
Caddy Dz <tbitw31@gmail.com>
2018-01-07 11:38:10 +0100

    Objective C Intro
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ ObjCTut2/main.m
@@ -0,0 +1,9 @@
+// The Foundation framework consists of fundamental classes used to develop Objective C programs
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
