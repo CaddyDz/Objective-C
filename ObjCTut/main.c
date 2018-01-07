2ce6a054e7d2d96153ae8a041d9c0188cf5bc33d
Caddy Dz <tbitw31@gmail.com>
2018-01-07 10:28:21 +0100

    Structs
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -3,44 +3,26 @@
 #include <float.h>
 #include <stdbool.h>
 
-// If you are being passed an address use * for the attribute
-void changeNumber(int *number) {
-    
-    // You assign a value to an address by using *
-    *number = 98765;
-}
+struct superHero {
+    char *realName;
+    char *superName;
+    float height;
+    float weight;
+};
 
 int main(int argc, const char * argv[]) {
     
-    // ------- Pointers --------
-    // Data is stored in memory at addresses
-    // Memory is like a bunch of boxes and the data type used defines how many boxes you need
+    // -------- Structs ------------
     
-    int randNum = 12345;
+    struct superHero superman;
     
-    // Get the address of a variable
-    printf("Location of random number : %p\n", &randNum);
+    superman.realName = "Klark Kent";
+    superman.superName = "SuperMan";
+    superman.height = 6.25;
+    superman.weight = 235;
     
-    // Store the address using the same data type and *
-    int *addrRandNum = &randNum;
+    printf("The superhero %s real name is %s, is %.2f ft high and weighs %.2f lbs\n", superman.superName, superman.realName, superman.height, superman.weight);
     
-    // Store a different value in the same location
-    *addrRandNum = 54321;
-    
-    printf("Value of random number : %d\n", *addrRandNum);
-    
-    // The memory location is the same with a new value
-    printf("Random number memory location : %p\n", &randNum);
-    
-    // Get the number of bytes for our integer
-    printf("Random number is %zu bytes in size\n", sizeof(randNum));
-    
-    // If you send the address of a var you can change its value in a function (Pass By Reference)
-    int number = 12345;
-    
-    changeNumber(&number);
-    
-    printf("New value of number is : %d\n", number);
-    
+    // Returning 0 means program exited correctly
     return 0;
 }
