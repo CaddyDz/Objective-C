e49f42cfc7947d790a9847535bb3a2790a3e3633
Caddy Dz <tbitw31@gmail.com>
2018-01-08 12:02:24 +0100

    NSString
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/main.m
+++ ObjCTut2/main.m
@@ -4,9 +4,22 @@ int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
         
-        // Works like printf
-        // More verbose as it echoes out the timestamp of compile time and object name
-        NSLog(@"Hello, World!");
+        NSString *nothing = nil;
+        
+        NSLog(@"Location of nil : %p\n", nothing);
+        
+        NSString *quote = @"Dogs have masters, while cats have staff";
+        
+        NSLog(@"Length of quote : %d\n", (int)[quote length]);
+        
+        NSLog(@"Character at index 5 : %c\n", [quote characterAtIndex:5]);
+        
+        char *name = "Grey";
+        
+        NSString *myName = [NSString stringWithFormat:@"- %s", name];
+        
+        BOOL isStringEqual = [quote isEqualToString:myName];
+        printf("Are strings equal : %d\n", isStringEqual);
     }
     return 0;
 }
