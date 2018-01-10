41fd12dc623e18715d6c2eba8d5418693c7e9b7d
Caddy Dz <tbitw31@gmail.com>
2018-01-10 15:15:18 +0100

    NSMutableString
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/main.m
+++ ObjCTut2/main.m
@@ -4,56 +4,13 @@ int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
         
-        // nil is used to define a nonexistent object
-        NSString *nothing = nil;
-        NSLog(@"Location of nil : %p\n", nothing);
+        // Create a mutable string with a starting capacity of 50 characters
+        NSMutableString *groceryList = [NSMutableString stringWithCapacity:50];
         
-        // Create a pointer to where the NSString object is
-        // A NSString can hold unicode characters
-        NSString *quote = @"Dogs have masters, while cats have staff";
+        // Append a value to the string
+        [groceryList appendFormat:@"%s", "Potato, Banana, Pasta"];
         
-        // You execute a method in the object you follow the object name with the method (Message)
-        NSLog(@"Length of quote : %d\n", (int)[quote length]);
-        
-        // Returns the data stored in NSString
-        NSLog(@"NSString : %@", quote);
-        
-        // Get the character at index 5
-        // You pass arguments after the :
-        NSLog(@"Character at index 5 : %c\n", [quote characterAtIndex:5]);
-        
-        // Use stringWithFormat to create a dynamic string
-        char *name = "Grey";
-        NSString *myName = [NSString stringWithFormat:@"- %s", name];
-        
-        // Test if 2 strings are equal
-        BOOL isStringEqual = [quote isEqualToString:myName];
-        printf("Are strings equal : %d\n", isStringEqual);
-        
-        // How to convert a NSString to a String
-        // How to nest messages
-        // Also available : lowercaseString, capitalizedString
-        const char *uCSString = [[myName uppercaseString]UTF8String];
-        printf("%s\n", uCSString);
-        
-        // How to combine strings
-        NSString *wholeQuote = [quote stringByAppendingString:myName];
-        
-        // Searching for strings
-        NSRange searchResult = [wholeQuote rangeOfString:@"Grey"];
-        if (searchResult.location == NSNotFound) {
-            NSLog(@"String not found");
-        } else {
-            printf("Grey is at index %lu and is %lu long\n", searchResult.location, searchResult.length);
-        }
-        
-        // Replace a substring by defining at what index to start and how many letters to replace
-        NSRange range = NSMakeRange(42, 4);
-        
-        const char *newQuote = [[wholeQuote stringByReplacingCharactersInRange:range withString:@"Anon"]UTF8String];
-        
-        printf("%s\n", newQuote);
-        
+        NSLog(@"groceryList : %@", groceryList);
     }
     return 0;
 }
