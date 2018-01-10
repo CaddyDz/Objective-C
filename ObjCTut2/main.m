c874f208183dee13b8d817afee77952c83d245d3
Caddy Dz <tbitw31@gmail.com>
2018-01-10 16:10:35 +0100

    Append, delete and insert in an NSString
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/main.m
+++ ObjCTut2/main.m
@@ -11,6 +11,22 @@ int main(int argc, const char * argv[]) {
         [groceryList appendFormat:@"%s", "Potato, Banana, Pasta"];
         
         NSLog(@"groceryList : %@", groceryList);
+        
+        
+        // Delete characters in a range (Start, Length)
+        [groceryList deleteCharactersInRange:NSMakeRange(0,8)];
+        
+        NSLog(@"groceryList : %@", groceryList);
+        
+        // Insert string at index
+        [groceryList insertString:@" ,Apple" atIndex:13];
+        
+        NSLog(@"groceryList : %@", groceryList);
+        
+        // Replace characters in a range
+        [groceryList replaceCharactersInRange:NSMakeRange(15, 5) withString:@"Orange"];
+        
+        NSLog(@"groceryList : %@", groceryList);
     }
     return 0;
 }
