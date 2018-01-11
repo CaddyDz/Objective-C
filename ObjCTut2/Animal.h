407cca26e70842096c61897eb3effa588812cc51
Caddy Dz <tbitw31@gmail.com>
2018-01-11 11:00:00 +0100

    Properties
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/Animal.h
+++ ObjCTut2/Animal.h
@@ -4,4 +4,16 @@
 // Define the properties and methods this class will have
 @interface Animal : NSObject
 
+// Define attributes of your objects
+// They can't be directly accessed, but getter and setter methods are automatically generated
+
+// You could put (readonly) before a property if you don't want a getter generated :
+// @property (readonly) NSString *name;
+@property NSString *name;
+@property NSString *favFood;
+@property NSString *sound;
+
+// Primitive type doesn't require a *
+@property float weight;
+
 @end
