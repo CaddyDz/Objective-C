341fc16933c3bc7db71cd2aadf2ce163666b0e85
Caddy Dz <tbitw31@gmail.com>
2018-01-11 10:27:50 +0100

    NSMutableArray
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/main.m
+++ ObjCTut2/main.m
@@ -4,22 +4,31 @@ int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
         
-        // Create an array
-        NSArray *officeSupplies = @[@"Pensils", @"Paper"];
+        // Create a mutable array and add objects
+        NSMutableArray *heroes = [NSMutableArray arrayWithCapacity:5];
         
-        NSLog(@"First item : %@", officeSupplies[0]);
+        [heroes addObject:@"Batman"];
+        [heroes addObject:@"Wonder Woman"];
+        [heroes addObject:@"Kid Flash"];
+        [heroes addObject:@"Flash"];
         
-        NSLog(@"Office Supplies : %@", officeSupplies);
+        // Insert into an index
+        [heroes insertObject:@"Superman" atIndex:2];
         
-        // Search for item in array
-        BOOL containsItem = [officeSupplies containsObject:@"Pensils"];
+        NSLog(@"%@", heroes);
         
-        NSLog(@"Need pensils : %d\n", containsItem);
+        // Remove objects
+        [heroes removeObject:@"Flash"];
         
-        // Number of items in array
-        NSLog(@"Total : %d\n", (int) [officeSupplies count]);
+        [heroes removeObjectAtIndex:0];
         
-        NSLog(@"Index of Pensils %lu", (unsigned long) [officeSupplies indexOfObject:@"Pensils"]);
+        [heroes removeObjectIdenticalTo:@"Superman" inRange:NSMakeRange(0, 2)];
+        
+        // Iterate through array
+        for (int i = 0; i < [heroes count]; i++) {
+            NSLog(@"Hero: %@", heroes[i]);
+        }
+        
     }
     return 0;
 }
