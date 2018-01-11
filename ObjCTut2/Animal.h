922ded3cbf40fa1510c9b88ec8a0ab1a17a36eea
Caddy Dz <tbitw31@gmail.com>
2018-01-11 10:39:30 +0100

    Header File
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/Animal.h
+++ ObjCTut2/Animal.h
@@ -1,13 +1,7 @@
-//
-//  Animal.h
-//  ObjCTut2
-//
-//  Created by CGP Grey on 1/11/18.
-//  Copyright © 2018 Salim Djerbouh. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
+// In Objective C your class is made up of an interface and implementation
+// Define the properties and methods this class will have
 @interface Animal : NSObject
 
 @end
