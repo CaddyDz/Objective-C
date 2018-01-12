a2af0c0832316396e11f24fffaaa5293e176152a
Caddy Dz <tbitw31@gmail.com>
2018-01-12 20:16:18 +0100

    Executing Object Methods
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/main.m
+++ ObjCTut2/main.m
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import "Animal.h"
 
 int main(int argc, const char * argv[]) {
     
@@ -9,6 +10,31 @@ int main(int argc, const char * argv[]) {
         // Allocate memory for an object and initialize it
         Animal *dog = [[Animal alloc] init];
         
+        // Call the method for our object
+        [dog getInfo];
+        
+        // How to get a value stored in an instance variable
+        NSLog(@"Dog's name is %@", [dog name]);
+        
+        // Set the value for an instance variable
+        [dog setName:@"Spot"];
+        
+        NSLog(@"Dog's name is %@", [dog name]);
+        
+        // Call the custom init
+        Animal *cat = [[Animal alloc] initWithName:@"Whiskers"];
+        
+        // You can also access variables with dot notation
+        NSLog(@"The cats name is %@", cat.name);
+        
+        // Call the method weightInKg
+        NSLog(@"180 lbs = %.2f kg", [dog weightInKg:180]);
+        
+        // Pass attributes to be added
+        NSLog(@"3 + 5 = %d", [dog getSum:3 nextNumber:5]);
+        
+        // Pass in an NSString
+        NSLog(@"%@", [dog talkToMe:@"Derek"]);
     }
     return 0;
 }
