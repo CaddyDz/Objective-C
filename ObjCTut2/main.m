5c8262def625da0104cd930ff5fed633c4528463
Caddy Dz <tbitw31@gmail.com>
2018-01-12 19:29:13 +0100

    Creating Objects
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- ObjCTut2/main.m
+++ ObjCTut2/main.m
@@ -4,7 +4,10 @@ int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
         
+        // -------- Objects ---------------
         
+        // Allocate memory for an object and initialize it
+        Animal *dog = [[Animal alloc] init];
         
     }
     return 0;
