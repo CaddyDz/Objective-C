05fec6df5516e85cad6d135cb36e2ed31eaff8b8
Caddy Dz <tbitw31@gmail.com>
2018-01-14 11:28:35 +0100

    Exercice 2
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -2,15 +2,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int value1, value2, sum;
-        
-        value1 = 50;
-        
-        value2 = 25;
-        
-        sum = value1 + value2;
-        
-        NSLog(@"Sum of %i and %i is %i",value1, value2, sum);
+        NSLog(@"\nIn Objective-C, lowercase letters are significant.\nmain is where program execution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
     }
     return 0;
 }
