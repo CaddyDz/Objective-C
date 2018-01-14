997f1c242706835eabbcc5ba7736ca18b4975d4b
Caddy Dz <tbitw31@gmail.com>
2018-01-14 11:53:52 +0100

    Exercise 6
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,28 +1,21 @@
-// Wrong Syntax
-/*
 #import <Foundation/Foundation.h>
-int main (int argc, const char *argv[]);
-(
-   @autoreleasepool {
-       INT sum;
-       /* COMPUTE RESULT //
-       sum = 25 + 37 - 19
-       / DISPLAY RESULTS /
-       NSLog (@'The answer is %i' sum);
-    }
-    return 0;
- }
-*/
 
-// Correct syntax
-#import <Foundation/Foundation.h>
-int main (int argc, const char * argv[]) {
+int main (int argc, const char *argv[]) {
+    
     @autoreleasepool {
-        int sum;
-        /* Compute result */
-        sum = 25 + 37 - 19;
-        /* Display results */
-        NSLog(@"The answer is %i", sum);
+        
+        int answer, result;
+        
+        answer = 100;
+        
+        result = answer - 10;
+        
+        NSLog (@"The result is %i\n", result + 5);
     }
+    
     return 0;
+    
+    /* Expected output
+     The result is 95
+    */
 }
