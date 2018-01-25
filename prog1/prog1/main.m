9087ad9d53ae52d16d58294df3a539f8433031c4
Caddy Dz <tbitw31@gmail.com>
2018-01-25 13:41:25 +0100

    Program to work with Fractions (class version)
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,12 +1,61 @@
-// Simple program to work with fractions
+// Simple program to work with fractions - class version
 #import <Foundation/Foundation.h>
+
+// ---- @interface section ----
+
+@interface Fraction : NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+// ---- @implementation section ----
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d
+{
+    denominator = d;
+}
+
+@end
+
+// ---- program section ----
 
 int main (int argc, const char *argv[]) {
     
     @autoreleasepool {
-        int numerator = 1;
-        int denominator = 3;
+        Fraction *myFraction;
+        // Create an instance of a Fraction
         
-        NSLog(@"The fraction is %i/%i", numerator, denominator);
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        // Set fraction to 1/3
+        
+        [myFraction setNumerator:1];
+        [myFraction setDenominator:3];
+        
+        // Display the fraction using the print method
+        
+        printf("The value of myFraction is:");
+        [myFraction print];
     }
+    
+    return 0;
 }
