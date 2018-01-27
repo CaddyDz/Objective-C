52cd49df0ea2c6ab3990578b531ce1e668b8ad80
Caddy Dz <tbitw31@gmail.com>
2018-01-27 20:03:39 +0100

    Exercise 2
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,14 +1,62 @@
-// Excercise
-
-int Int = 1; // Valid because the reserved keyword is "int" and Objective C is case sensitive
-int _calloc = 1; // Valid because it starts with an underscore
-int clearScreen = 1; // Valid because it uses camelCase
-int ReInitialize = 1; // Valid because it uses PascalCase
-int playNextSong = 1; // Valid because is uses camelCase
-int Xx = 1; // Valid because it uses PascalCase
-int _1312 = 1; // Valid because it starts with an underscore
-int _ = 1; // Valid because it starts with an underscore
-int 6_05 = 1; // Invalid because it starts with a digit
-int alphaBetaRoutine = 1; // Valid because it uses camelCase
-int z = 1; // Valid because it uses a character
-int A$ = 1; // Valid because it starts with a legal character
+// Object is phone
+#import <Foundation/Foundation.h>
+// ---- @interface section ----
+@interface Phone: NSObject
+-(void) call: (int) nb;
+-(void) text: (char*) n;
+-(void) chat: (char*) r;
+-(void) listen: (char*) s;
+-(void) trackTime: (char*) a;
+@end
+// ---- @implementation section ----
+@implementation Phone
+{
+    int number;
+    char* name;
+    char* recepient;
+    char* song;
+    char* activity;
+}
+-(void) call: (int) nb
+{
+    number = nb;
+    NSLog(@"Calling: %i", number);
+}
+-(void) text: (char*) n
+{
+    name = n;
+    NSLog(@"Texting: %s", name);
+}
+-(void) chat: (char*) r
+{
+    recepient = r;
+    NSLog(@"Chatting with: %s", recepient);
+}
+-(void) listen: (char*) s
+{
+    song = s;
+    NSLog(@"Listening to song: %s", song);
+}
+-(void) trackTime: (char*) a
+{
+    activity = a;
+    NSLog(@"Tracking activity: %s", activity);
+}
+@end
+// ---- program section ----
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Phone *nexus;
+        
+        nexus = [Phone alloc];
+        nexus = [nexus init];
+        
+        [nexus call:550647548];
+        [nexus text:"Ilhem"];
+        [nexus chat:"Ihab"];
+        [nexus listen:"Never Find"];
+        [nexus trackTime:"Coding"];
+        
+    }
+    return 0;
+}
