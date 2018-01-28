175a49073937f9c4dcb2f6ac684f90ffd93172a5
Caddy Dz <tbitw31@gmail.com>
2018-01-28 11:16:21 +0100

    Chapter 4 data types and expressions Program 4.1
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,58 +1,17 @@
-// Cartesian cooredinates program
+// Use the basic Objective-C data types
 #import <Foundation/Foundation.h>
-/* XYPoint class */
-// ---- @interface section ----
-@interface XYPoint: NSObject
 
-// Define getters and setters
--(void) setX: (int) x; // Setters need to be passed an argument and return nothing
--(void) setY: (int) y;
--(int) getX; // Getters don't need any arguments and they return an integer value
--(int) getY;
-
-@end
-// ---- @implementation section ----
-@implementation XYPoint
+int main(int argc, char *argv[])
 {
-    // Define instance variables
-    int x_coordinate;
-    int y_coordinate;
-}
--(void) setX:(int) x
-{
-    x_coordinate = x;
-}
--(void) setY:(int) y
-{
-    y_coordinate = y;
-}
--(int) getX
-{
-    return x_coordinate;
-}
--(int) getY
-{
-    return y_coordinate;
-}
-
-@end
-// ---- program section ----
-int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // Declaring objects
-        XYPoint *getPos;
-        
-        // Allocating memory to objects and initializing them
-        getPos = [XYPoint alloc];
-        getPos = [getPos init];
-        
-        // Set X coordinate
-        [getPos setX:367486001];
-        
-        // Set Y coordinate
-        [getPos setY:30698291];
-        
-        NSLog(@"You are in %i X coordinate and %i Y coordinate", [getPos getX], [getPos getY]);
+        int integerVar = 100;
+        float floatingVar = 331.79;
+        double doubleVar = 8.44e+11;
+        char charVar = 'W';
+        NSLog(@"integerVar = %i", integerVar);
+        NSLog(@"floatingVar = %f", floatingVar);
+        NSLog(@"doubleVar = %e", doubleVar);
+        NSLog(@"doubleVar = %g", doubleVar);
+        NSLog(@"charVar = %c", charVar);
     }
-    return 0;
 }
