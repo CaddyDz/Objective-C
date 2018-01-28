04feeb211349fec5aba040a92a28c8ec28dad18c
Caddy Dz <tbitw31@gmail.com>
2018-01-28 11:30:27 +0100

    Operator precedence
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,17 +1,28 @@
-// Use the basic Objective-C data types
+// Illustrate the use of various arithmetic operators
 #import <Foundation/Foundation.h>
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        int integerVar = 100;
-        float floatingVar = 331.79;
-        double doubleVar = 8.44e+11;
-        char charVar = 'W';
-        NSLog(@"integerVar = %i", integerVar);
-        NSLog(@"floatingVar = %f", floatingVar);
-        NSLog(@"doubleVar = %e", doubleVar);
-        NSLog(@"doubleVar = %g", doubleVar);
-        NSLog(@"charVar = %c", charVar);
+        int a = 100;
+        int b = 2;
+        int c = 25;
+        int d = 4;
+        int result;
+        
+        result = a - b; // substraction
+        NSLog(@"a - b = %i", result);
+        
+        result = b * c; // multiplication
+        NSLog(@"b * c = %i", result);
+        
+        result = a / c; // division
+        NSLog(@"a / c = %i", result);
+        
+        result = a + b * c; // precedence
+        NSLog(@"a + c * c = %i", result);
+        
+        NSLog(@"a * b + c * d = %i", a * b + c * d);
     }
+    return 0;
 }
