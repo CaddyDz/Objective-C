8f2c4b96bc32796357f31c0a9a62de4d6a30638f
Caddy Dz <tbitw31@gmail.com>
2018-01-28 11:04:24 +0100

    Exercise 7
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,153 +1,38 @@
+// Cartesian cooredinates program
 #import <Foundation/Foundation.h>
-/*
- Object is boat
- action: sail, dock, prep, getGas, service
- */
+/* XYPoint class */
 // ---- @interface section ----
-@interface Boat: NSObject
--(void) sail: (char*) se;
--(void) dock: (char*) pt;
--(void) prep: (char*) gg;
--(void) getGas: (char*) st;
--(void) service: (int) t;
+@interface XYPoint: NSObject
+
+// Define getters and setters
+-(void) setX: (int) x; // Setters need to be passed an argument and return nothing
+-(void) setY: (int) y;
+-(int) getX; // Getters don't need any arguments and they return an integer value
+-(int) getY;
+
 @end
 // ---- @implementation section ----
-@implementation Boat
+@implementation XYPoint
 {
-    char* sea;
-    char* port;
-    char* garage;
-    char* station;
-    int time;
+    // Define instance variables
+    int x_coordinate;
+    int y_coordinate;
 }
--(void) sail: (char*) se // Represents "sea" string argument
+-(void) setX:(int) x
 {
-    sea = se;
-    NSLog(@"Sailing into the %s sea", sea);
+    x_coordinate = x;
 }
--(void) dock: (char*) pt // Represents "dock" string argument
+-(void) setY:(int) y
 {
-    port = pt;
-    NSLog(@"Docking the boat in %s port", pt);
+    y_coordinate = y;
 }
-// Prepare method
--(void) prep: (char*) gg // Represents "garage" string argument
+-(int) getX
 {
-    garage = gg;
-    NSLog(@"Repairing boat in %s garage", garage);
+    return x_coordinate;
 }
--(void) getGas:(char *) st // Represents "station" string argument
+-(int) getY
 {
-    station = st;
-    NSLog(@"Refilling fuel on %s station", station);
-}
--(void) service: (int) t // Represents "time" string argument
-{
-    time = t;
-    NSLog(@"Delivering service in %i", t);
-}
-@end
-/*
- Object is motorcycle
- action: anchor, stand, prep, getGas, service
- */
-// ---- @interface section ----
-@interface Bike: NSObject
--(void) anchor: (char*) sp;
--(void) stand: (int) i;
--(void) prep: (char*) gg;
--(void) getGas: (char*) st;
--(void) service: (int) t;
-@end
-// ---- @implementation section ----
-@implementation Bike
-{
-    char* spot;
-    int interval;
-    char* garage;
-    char* station;
-    int time;
-}
--(void) anchor: (char*) sp // Represents "spot" string argument
-{
-    spot = sp;
-    NSLog(@"Anchoring on %s spot", spot);
-}
--(void) stand: (int) i // Represents "interval" integer argument
-{
-    interval = i;
-    NSLog(@"Performing a stand for %i seconds", interval);
-}
-// Prepare method
--(void) prep: (char*) gg // Represents "garage" string argument
-{
-    garage = gg;
-    NSLog(@"Repairing bike in %s garage", garage);
-}
--(void) getGas:(char *) st // Represents "station" string argument
-{
-    station = st;
-    NSLog(@"Refilling fuel on %s station", station);
-}
--(void) service: (int) t // Represents "time" string argument
-{
-    time = t;
-    NSLog(@"Delivering service in %i", t);
-}
-@end
-/*
- Object is a car
- actions: accelerate, break, park, prep, getGas, service
- */
-// ---- @interface section ----
-@interface Car: NSObject
--(void) accelerate: (int) sd;
--(void) Break: (int) lv; // break is a reserved keyword so am just exploiting case sensitivity for readability
--(void) park: (char*) sp;
--(void) prep: (char*) gg;
--(void) getGas: (char*) st;
--(void) service: (int) t;
-@end
-// ---- @implementation section ----
-@implementation Car
-{
-    int speed;
-    int level;
-    char* spot;
-    char* garage;
-    char* station;
-    int time;
-}
--(void) accelerate:(int) sd
-{
-    speed = sd;
-    NSLog(@"Accelerating to %i MPH", speed);
-}
--(void) Break: (int) lv
-{
-    level = lv;
-    NSLog(@"Breaking at level %i", level);
-}
--(void) park: (char*) sp // Represents "spot" string argument
-{
-    spot = sp;
-    NSLog(@"Parking on %s spot", spot);
-}
-// Prepare method
--(void) prep: (char*) gg // Represents "garage" string argument
-{
-    garage = gg;
-    NSLog(@"Repairing car in %s garage", garage);
-}
--(void) getGas:(char *) st // Represents "station" string argument
-{
-    station = st;
-    NSLog(@"Refilling fuel on %s station", station);
-}
--(void) service: (int) t // Represents "time" string argument
-{
-    time = t;
-    NSLog(@"Delivering service in %i", t);
+    return y_coordinate;
 }
 
 @end
@@ -155,43 +40,19 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Declaring objects
-        Boat *boat;
-        Bike *motorcycle;
-        Car *car;
+        XYPoint *getPos;
         
         // Allocating memory to objects and initializing them
-        boat = [Boat alloc];
-        boat = [boat init];
-        motorcycle = [Bike alloc];
-        motorcycle = [motorcycle init];
-        car = [Car alloc];
-        car = [car init];
+        getPos = [XYPoint alloc];
+        getPos = [getPos init];
         
-        /* Calling objects's methods and passing random parameters */
+        // Set X coordinate
+        [getPos setX:367486001];
         
-        // Boat methods
-        [boat sail:"Mediterranean"];
-        [boat dock:"Bora Bora"]; // Triple X movie xD (damn my memory)
-        [boat prep:"My MA"];
-        [boat getGas:"Sydney Harbour"];
-        [boat service:2018]; // Delievering service in 2018
+        // Set Y coordinate
+        [getPos setY:30698291];
         
-        // Bike methods
-        [motorcycle anchor:"Last"]; // Anchor the bike on the last spot
-        [motorcycle stand:20]; // Stand for 20 seconds
-        [motorcycle prep:"Matt Ferreira"];
-        [motorcycle getGas:"Exxon"];
-        [motorcycle service:2018]; // Delievering service in 2018
-        
-        // Car methods
-        [car accelerate:120];
-        [car Break:5]; // This will almost certainly kill you with that speed (just saying)
-        [car park:"First"]; // Park car on first visible size suitable empty spot
-        [car prep:"Tej"]; // The fast and the furious repairing cars garage
-        [car getGas:"Castrol"];
-        [motorcycle service:2018]; // Delievering service in 2018
-        
-        NSLog(@"Yes there is an overlap between the objects");
+        NSLog(@"You are in %i X coordinate and %i Y coordinate", [getPos getX], [getPos getY]);
     }
     return 0;
 }
