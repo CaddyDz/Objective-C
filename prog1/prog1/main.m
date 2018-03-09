a3d39c65971a0d72b5a03fa3bec54f4664435648
Caddy Dz <tbitw31@gmail.com>
2018-03-09 14:21:11 +0100

    Exercise 6
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,14 +1,80 @@
-// Exercise 5
-
 #import <Foundation/Foundation.h>
 
-int main (int argc, char * argv[])
+/*
+ * Complex numbers are numbers that contain two components: a real part and imaginary part.
+ * If a is the real component and b is the imaginary component, this notation is used to represent the number:
+ * a + b i
+ */
+
+/*
+ * Write an Objective-C program that defines a new class called Complex.
+ * Define the methods for the new class.
+ * Write a test program to test the new class and its methods.
+ */
+
+// ----- @interface section -----
+
+@interface Real : NSObject
+
+-(void) setReal: (double) a;
+-(void) setImaginary: (double) b;
+-(void) print; // display as a + bi
+-(double) real;
+-(double) imaginary;
+
+@end
+
+// ----- @implementation section -----
+
+@implementation Real
+
+{
+    double real;
+    double imaginary ;
+}
+
+-(void) print
+{
+    NSLog(@"%f + %f i\n", real, imaginary);
+}
+
+-(void) setReal: (double) a
+{
+    real = a;
+}
+
+-(void) setImaginary: (double) b
+{
+    imaginary = b;
+}
+
+-(double) real
+{
+    return real;
+}
+
+-(double) imaginary
+{
+    return imaginary;
+}
+
+@end
+
+// ----- Program section -----
+
+int main (int argc, const char *argv[])
 {
     @autoreleasepool {
- 
-        int result = ((3.21 * (10 * -8)) + (2.01 * (10-7))) / ((7.16 * (10 * -6)) + (2.01 * (10 * -8)));
+        Real *real;
+        real = [[Real alloc] init];
         
-        NSLog(@"Result is %i\n", result);
+        [real setReal:1345];
+        [real setImaginary:355];
+        
+        [real print];
+        
     }
     return 0;
 }
+
+
