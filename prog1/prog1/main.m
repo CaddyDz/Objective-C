ab25439e19f30122dcec2bc35fd303dfb829dffb
Caddy Dz <tbitw31@gmail.com>
2018-03-16 09:47:50 +0100

    5. Program Looping (Program 5.1) calculate 8th triangular number manually
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,131 +1,15 @@
-// Implement a Calculator class
-
 #import <Foundation/Foundation.h>
 
-@interface Calculator: NSObject
-
-// Accumulator methods
--(void) setAccumulator: (double) value;
--(void) clear;
--(double) accumulator;
-
-// Arithmetic methods
--(double) add: (double) value;
--(double) substract: (double) value;
--(double) multiply: (double) value;
--(double) divide: (double) value;
-
-// Added methods
--(double) changeSign; // Change sign of accumulator
--(double) reciprocal; // 1 / Accumulator
--(double) xSquared; // Accumulator squared
-
-// Memory methods
--(double) memoryClear; // Clear memory
--(double) memoryStore; // Set memory to accumulator
--(double) memoryRecall; // Set accumulator to memory
--(double) memoryAdd: (double) value; // Add value into memory
--(double) memorySubject: (double) value; // Substract value from memory
-
-@end
-
-@implementation Calculator
-
-{
-    double accumulator;
-}
-
--(void) setAccumulator: (double) value
-{
-    accumulator = value;
-}
-
--(void) clear
-{
-    accumulator = 0;
-}
-
--(double) accumulator
-{
-    return accumulator;
-}
-
--(double) add: (double) value
-{
-    accumulator += value;
-    return accumulator;
-}
-
--(double) substract: (double) value
-{
-    accumulator -= value;
-    return accumulator;
-}
-
--(double) multiply: (double) value
-{
-    accumulator *= value;
-    return accumulator;
-}
-
--(double) divide: (double) value
-{
-    accumulator /= value;
-    return accumulator;
-}
-
--(double) changeSign
-{
-    return -accumulator;
-}
-
--(double) reciprocal
-{
-    return 1 / accumulator;
-}
-
--(double) xSquared
-{
-    return accumulator * accumulator;
-}
-
--(double) memoryClear
-{
-    return 0;
-}
-
--(double) memoryStore
-{
-    double memory = accumulator;
-    return memory;
-}
-
--(double) memoryRecall
-{
-    return memory;
-}
-
--(double) memoryAdd:(double)value
-{
-    
-}
-
-@end
+// Program to calculate the eighth triangular number
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        Calculator *deskCalc = [[Calculator alloc] init];
+        int triangularNumber;
         
-        [deskCalc setAccumulator: 100.0];
-        [deskCalc add: 200.];
-        [deskCalc substract: 10.0];
-        [deskCalc multiply: 5];
-        [deskCalc divide: 2];
-        [deskCalc changeSign];
-        [deskCalc reciprocal];
-        [deskCalc xSquared];
-        NSLog(@"The result is %g", [deskCalc accumulator]);
+        triangularNumber = 1 + 2 + 3 + 4 + 6 + 7 + 8;
+        
+        NSLog(@"The eighth triangular number is %i", triangularNumber);
     }
     return 0;
 }
