4d8d95f3859aeab2ddab9176ff9af5488ca748ef
Caddy Dz <tbitw31@gmail.com>
2018-03-16 09:38:21 +0100

    Exercise 10
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -20,6 +20,13 @@
 -(double) reciprocal; // 1 / Accumulator
 -(double) xSquared; // Accumulator squared
 
+// Memory methods
+-(double) memoryClear; // Clear memory
+-(double) memoryStore; // Set memory to accumulator
+-(double) memoryRecall; // Set accumulator to memory
+-(double) memoryAdd: (double) value; // Add value into memory
+-(double) memorySubject: (double) value; // Substract value from memory
+
 @end
 
 @implementation Calculator
@@ -80,6 +87,27 @@
 -(double) xSquared
 {
     return accumulator * accumulator;
+}
+
+-(double) memoryClear
+{
+    return 0;
+}
+
+-(double) memoryStore
+{
+    double memory = accumulator;
+    return memory;
+}
+
+-(double) memoryRecall
+{
+    return memory;
+}
+
+-(double) memoryAdd:(double)value
+{
+    
 }
 
 @end
