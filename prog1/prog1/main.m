a01ab9afe29473c5c7b907f8ca9b61d6928f969a
Caddy Dz <tbitw31@gmail.com>
2018-03-17 17:25:13 +0100

    GCD Algorithm
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,15 +1,24 @@
-// This program introduces the while statement
+// Find the greatest common divisor of two nonnegative integers
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        int count = 1;
-        while(count <= 5) {
-            NSLog(@"%i", count);
-            ++count;
+        unsigned int u, v, temp;
+        
+        NSLog(@"Please type in two nonnegative integers.");
+        
+        scanf("%u%u", &u, &v);
+        
+        while (v != 0) {
+            temp = u % v;
+            u = v;
+            v = temp;
         }
+        
+        NSLog(@"Their greatest common divisor is %u", u);
     }
+    
     return 0;
 }
