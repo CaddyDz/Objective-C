d99e59989ff8d88834ded876f74b4069bcaf17af
Caddy Dz <tbitw31@gmail.com>
2018-03-17 17:59:16 +0100

    Exercise 1
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,21 +1,21 @@
-// Program to reverse the digits of a number
+/* Exercise 1
+ * A program to generate and display a table of n and n²
+ * where n is an integer ranging from 1 through 10
+ */
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        int number, right_digit;
         
-        NSLog(@"Enter your number.");
-        scanf("%i", &number);
-        
-        do {
-            right_digit = number % 10;
-            NSLog(@"%i", right_digit);
-            number /= 10;
+        int n;
+        NSLog(@"TABLE OF POWER INTEGERS");
+        NSLog(@" n                n²   ");
+        NSLog(@"---           ---------");
+        for (n = 0; n <= 10; n++) {
+            NSLog(@"%2i                %i", n, n * n);
         }
-        while (number != 0);
     }
     return 0;
 }
