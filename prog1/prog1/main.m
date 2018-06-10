af2bc63132b5242709219fe48e1dd80fecf7344a
Salim Djerbouh <github@caddydz.me>
2018-06-10 12:15:01 +0100

    Second version using BOOL type and predefined values
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,20 +1,22 @@
 // Program to generate a table of prime numbers
+// Second version using BOOL type and predefined values
 
 #import <Foundation/Foundation.h>
 
 int main (int argc, char * argv[])
 {
     @autoreleasepool {
-        int p, d, isPrime;
+        int p, d;
+        BOOL isPrime;
         
         for (p = 2; p <= 50; ++p) {
-            isPrime = 1;
+            isPrime = YES;
             
             for (d = 2; d < p; ++d)
                 if (p % d == 0)
-                    isPrime = 0;
+                    isPrime = NO;
                 
-                if (isPrime != 0)
+                if (isPrime == YES)
                     NSLog(@"%i", p);
         }
     }
