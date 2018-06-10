5a35f164b96e4e96b7a82fb70505960b1d77b35c
Salim Djerbouh <github@caddydz.me>
2018-06-10 14:52:46 +0100

    Synthesized Accessor Methods
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/Fraction.m
+++ FractionTest/FractionTest/Fraction.m
@@ -9,34 +9,11 @@
 #import "Fraction.h"
 
 @implementation Fraction
-{
-    int numerator;
-    int denominator;
-}
+@synthesize numerator, denominator;
 
 -(void) print
 {
     NSLog(@"%i/%i", numerator, denominator);
-}
-
--(void) setNumerator: (int) n
-{
-    numerator = n;
-}
-
--(void) setDenominator: (int) d
-{
-    denominator = d;
-}
-
--(int) numerator
-{
-    return numerator;
-}
-
--(int) denominator
-{
-    return denominator;
 }
 
 -(double) convertToNum
@@ -47,4 +24,5 @@
         return NAN;
     }
 }
+
 @end
