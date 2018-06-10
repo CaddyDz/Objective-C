9350f84618f2ff15e43d3a130cb58cb101c31102
Salim Djerbouh <github@caddydz.me>
2018-06-10 12:51:21 +0100

    Exercise 2
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,20 +1,92 @@
-// Exercise 1
+// Exercise 2
+
+// Implement a Calculator class
 
 #import <Foundation/Foundation.h>
+
+@interface Calculator: NSObject
+
+// accumulator methods
+-(void) setAccumulator: (double) value;
+-(void) clear;
+-(double) accumulator;
+
+// arithmetic methods
+-(void) add: (double) value;
+-(void) substract: (double) value;
+-(void) multiply: (double) value;
+-(void) divide: (double) value;
+@end
+
+@implementation Calculator
+{
+    double accumulator;
+}
+
+-(void) setAccumulator: (double) value
+{
+    accumulator = value;
+}
+
+-(void) clear
+{
+    accumulator = 0;
+}
+
+-(double) accumulator
+{
+    return accumulator;
+}
+
+-(void) add: (double) value
+{
+    accumulator += value;
+}
+
+-(void) substract: (double) value
+{
+    accumulator -= value;
+}
+
+-(void) multiply: (double) value
+{
+    accumulator *= value;
+}
+
+-(void) divide: (double) value
+{
+    if (value != 0.0)
+        accumulator /= value;
+    else {
+        NSLog(@"Division by zero.");
+        accumulator = NAN;
+    }
+}
+@end
 
 int main (int argc, char * argv[])
 {
     @autoreleasepool {
-        int val1, val2;
+        double value1, value2;
+        char operator;
+        Calculator *deskCalc = [[Calculator alloc] init];
+        NSLog(@"Type in your expression.");
+        scanf("%lf %c %lf", &value1, &operator, &value2);
         
-        NSLog(@"Please type in two numbers: ");
-        scanf("%i", &val1);
-        scanf("%i", &val2);
-        if (val1 % val2) {
-            NSLog(@"%i is not evenly divisible by %i", val1, val2);
+        [deskCalc setAccumulator: value1];
+        if (operator == '+') {
+            [deskCalc add: value2];
+        } else if (operator == '-') {
+            [deskCalc substract: value2];
+        } else if (operator == '*') {
+            [deskCalc multiply: value2];
+        } else if (operator == '/') {
+            [deskCalc divide: value2];
         } else {
-            NSLog(@"%i is evenly divisible by %i", val1, val2);
+            NSLog(@"Unknown operator.");
+            return 0;
         }
+        NSLog(@"%.2f", [deskCalc accumulator]);
     }
     return 0;
 }
