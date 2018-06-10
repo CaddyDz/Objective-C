c11aaa3215eacfff25ee62af76da3b8d077da3d1
Salim Djerbouh <github@caddydz.me>
2018-06-10 10:09:01 +0100

    Program 6.2 Fractions
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,18 +1,73 @@
-// Calculate the absolute value of an integer
-
 #import <Foundation/Foundation.h>
+
+@interface Fraction : NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenomerator: (int) d;
+-(int) numerator;
+-(int) denominator;
+-(double) convertToNum;
+
+@end
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+    numerator = n;
+}
+
+-(void) setDenomerator: (int) d
+{
+    denominator = d;
+}
+
+-(int) numerator
+{
+    return numerator;
+}
+
+-(int) denominator
+{
+    return denominator;
+}
+
+-(double) convertToNum
+{
+    if (denominator != 0) {
+        return (double) numerator / denominator;
+    } else {
+        // Not A Number
+        return NAN;
+    }
+}
+
+@end
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        int number;
+        Fraction *aFraction = [[Fraction alloc] init];
+        Fraction *bFraction = [[Fraction alloc] init];
+        [aFraction setNumerator:1]; // 1st fraction is 1/4
+        [aFraction setDenomerator:4];
+        [aFraction print];
+        NSLog(@" =");
+        NSLog(@"%g", [aFraction convertToNum]);
         
-        NSLog(@"Type in your number: ");
-        scanf("%i", &number);
-        if (number < 0) {
-            number = -number;
-            NSLog(@"The absolute value is %i", number);
-        }
-        return 0;
+        [bFraction print]; // never assigned a value
+        NSLog(@" =");
+        NSLog(@"%g", [bFraction convertToNum]);
     }
+    return 0;
 }
