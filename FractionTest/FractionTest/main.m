892fd2500f3d4934c6f263a64a0fbb34075430ef
Salim Djerbouh <github@caddydz.me>
2018-06-10 17:00:02 +0100

    Allocating and Returning Objects from Methods
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -6,7 +6,7 @@ int main(int argc, char * argv[])
         Fraction *aFraction = [[Fraction alloc] init];
         Fraction *bFraction = [[Fraction alloc] init];
         
-        // Set two fractions to 1/4 and 1/2 and add them together
+        Fraction *resultFraction;
         
         [aFraction setTo:1 over:4]; // set 1st fraction to 1/4
         [bFraction setTo:1 over:2]; // set 2nd fraction to 1/2
@@ -16,8 +16,8 @@ int main(int argc, char * argv[])
         [bFraction print];
         NSLog(@"=");
         
-        [aFraction add: bFraction];
-        [aFraction print];  
+        resultFraction = [aFraction add: bFraction];
+        [resultFraction print];
     }
     return 0;
 }
