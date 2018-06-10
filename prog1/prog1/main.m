0cc48e1ab124d7e63302debb6b8a842d79a3a663
Salim Djerbouh <github@caddydz.me>
2018-06-10 10:18:12 +0100

    Program 6.3 even or odd
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -1,73 +1,21 @@
+// Program to determine if a number is even or odd
+
 #import <Foundation/Foundation.h>
-
-@interface Fraction : NSObject
-
--(void) print;
--(void) setNumerator: (int) n;
--(void) setDenomerator: (int) d;
--(int) numerator;
--(int) denominator;
--(double) convertToNum;
-
-@end
-
-@implementation Fraction
-{
-    int numerator;
-    int denominator;
-}
-
--(void) print
-{
-    NSLog(@"%i/%i", numerator, denominator);
-}
-
--(void) setNumerator: (int) n
-{
-    numerator = n;
-}
-
--(void) setDenomerator: (int) d
-{
-    denominator = d;
-}
-
--(int) numerator
-{
-    return numerator;
-}
-
--(int) denominator
-{
-    return denominator;
-}
-
--(double) convertToNum
-{
-    if (denominator != 0) {
-        return (double) numerator / denominator;
-    } else {
-        // Not A Number
-        return NAN;
-    }
-}
-
-@end
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        Fraction *aFraction = [[Fraction alloc] init];
-        Fraction *bFraction = [[Fraction alloc] init];
-        [aFraction setNumerator:1]; // 1st fraction is 1/4
-        [aFraction setDenomerator:4];
-        [aFraction print];
-        NSLog(@" =");
-        NSLog(@"%g", [aFraction convertToNum]);
+        int number_to_test, remainder;
         
-        [bFraction print]; // never assigned a value
-        NSLog(@" =");
-        NSLog(@"%g", [bFraction convertToNum]);
+        NSLog(@"Enter your number to be tested: ");
+        scanf("%i", &number_to_test);
+        remainder = number_to_test % 2;
+        
+        if (remainder == 0)
+            NSLog(@"The number is even.");
+        
+        if (remainder != 0)
+            NSLog(@"The number is odd.");
     }
     return 0;
 }
