3cda8cb369b1b9ca7769ad47194a2e025c587020
Salim Djerbouh <github@caddydz.me>
2018-06-17 15:57:34 +0100

    Program 8.1 Simple example to illustrate inheritance
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,23 +1,46 @@
-#import "Fraction.h"
+// Simple example to illustrate inheritance
+
+#import <Foundation/Foundation.h>
+
+// ClassA declaration and definition
+
+@interface ClassA: NSObject
+{
+    int x;
+}
+
+-(void) initVar;
+@end
+
+@implementation ClassA
+-(void) initVar
+{
+    x = 100;
+}
+@end
+
+// Class B declaration and definition
+
+@interface ClassB : ClassA
+-(void) printVar;
+@end
+
+@implementation ClassB
+
+-(void) printVar
+{
+    NSLog(@"x = %i", x);
+}
+
+@end
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        Fraction *aFraction = [[Fraction alloc] init];
-        Fraction *bFraction = [[Fraction alloc] init];
+        ClassB *b = [[ClassB alloc] init];
         
-        Fraction *resultFraction;
-        
-        [aFraction setTo:1 over:4]; // set 1st fraction to 1/4
-        [bFraction setTo:1 over:2]; // set 2nd fraction to 1/2
-        
-        [aFraction print];
-        NSLog(@"+");
-        [bFraction print];
-        NSLog(@"=");
-        
-        resultFraction = [aFraction add: bFraction];
-        [resultFraction print];
+        [b initVar]; // will use inherited method
+        [b printVar]; // reveal value of x
     }
     return 0;
 }
