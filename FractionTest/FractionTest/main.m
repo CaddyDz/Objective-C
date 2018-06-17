550ba75861f6ad49481d102e2d136935d1f7c96f
Salim Djerbouh <github@caddydz.me>
2018-06-17 16:21:40 +0100

    Program 8.2
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,46 +1,15 @@
-// Simple example to illustrate inheritance
+#import "Rectangle.h"
 
-#import <Foundation/Foundation.h>
-
-// ClassA declaration and definition
-
-@interface ClassA: NSObject
-{
-    int x;
-}
-
--(void) initVar;
-@end
-
-@implementation ClassA
--(void) initVar
-{
-    x = 100;
-}
-@end
-
-// Class B declaration and definition
-
-@interface ClassB : ClassA
--(void) printVar;
-@end
-
-@implementation ClassB
-
--(void) printVar
-{
-    NSLog(@"x = %i", x);
-}
-
-@end
-
-int main(int argc, char * argv[])
+int main (int argc, char * argv[])
 {
     @autoreleasepool {
-        ClassB *b = [[ClassB alloc] init];
+        Rectangle *myRect = [[Rectangle alloc] init];
         
-        [b initVar]; // will use inherited method
-        [b printVar]; // reveal value of x
+        [myRect setWidth:5 andHeight:8];
+        
+        NSLog(@"Rectangle: w = %i, h = %i", myRect.width, myRect.height);
+        NSLog(@"Area = %i, Perimeter = %i", [myRect area], [myRect permiter]);
+        
     }
     return 0;
 }
