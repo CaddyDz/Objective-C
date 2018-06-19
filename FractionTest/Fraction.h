75ff285980a8abb14730fd615cd2b059b6388ebd
Salim Djerbouh <github@caddydz.me>
2018-06-19 09:59:28 +0100

    Program 9.4 no such method
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

---
+++ FractionTest/Fraction.h
@@ -0,0 +1,21 @@
+//
+//  Fraction.h
+//  FractionTest
+//
+//  Created by Caddy DZ on 6/19/18.
+//  Copyright © 2018 Caddy DZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fraction : NSObject
+
+@property int numerator, denominator;
+
+-(void) print;
+-(void) setTo: (int) n over: (int) d;
+-(double) convertToNum;
+-(Fraction *) add: (Fraction *) f;
+-(void) reduce;
+
+@end
