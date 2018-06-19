615b003aa9c7298eaff78b172ca5ee62f4d13788
Salim Djerbouh <github@caddydz.me>
2018-06-19 10:01:59 +0100

    Program 9.5 Exception Handling
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,10 +1,15 @@
-#import "Fraction.h"
+#import <Foundation/Foundation.h>
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        Fraction *f = [[Fraction alloc] init];
-        [f noSuchMethod];
+        NSArray *myArray = [NSArray array];
+        
+        @try {
+            [myArray objectAtIndex:2];
+        } @catch (NSException *exception) {
+            NSLog(@"Caught %@%@", exception.name, exception.reason);
+        }
         NSLog(@"Execution continues");
     }
     return 0;
