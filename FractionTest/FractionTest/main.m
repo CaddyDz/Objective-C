7e60c1b73bd5c8b9031bc77de07e58c8864ac90b
Salim Djerbouh <github@caddydz.me>
2018-06-24 21:20:37 +0100

    Program 13.1 generate first 15 Fibonacci numbers
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,91 +1,22 @@
-#import "Fraction.h"
+// Program to generate the first 15 Fibonacci numbers
 
-@interface Fraction (MathOps)
--(Fraction *) add: (Fraction *) f;
--(Fraction *) mul: (Fraction *) f;
--(Fraction *) sub: (Fraction *) f;
--(Fraction *) div: (Fraction *) f;
-@end
-
-@implementation Fraction (MathOps)
--(Fraction *) add: (Fraction *) f
-{
-    // To add two fraction:
-    // a/b + c/d = ((a*d) + (b*c)) / (d * b)
-    
-    Fraction *result = [[Fraction alloc] init];
-    
-    result.numerator = (self.numerator * f.denominator) + (self.denominator * f.numerator);
-    result.denominator = self.denominator * f.denominator;
-    [result reduce];
-    return result;
-}
-
--(Fraction *) sub: (Fraction *) f
-{
-    // To sub two fractions:
-    // a/b - c/d = ((a*d) - (b*c)) / (b * d)
-    
-    Fraction *result = [[Fraction alloc] init];
-    
-    result.numerator = (self.numerator * f.denominator) - (self.denominator * f.numerator);
-    result.denominator = self.denominator * f.denominator;
-    [result reduce];
-    
-    return result;
-}
-
--(Fraction *) mul: (Fraction *) f
-{
-    Fraction *result = [[Fraction alloc] init];
-    
-    result.numerator = self.numerator * f.numerator;
-    result.denominator = self.denominator * f.denominator;
-    [result reduce];
-    
-    return result;
-}
-
--(Fraction *) div:(Fraction *)f
-{
-    Fraction *result = [[Fraction alloc] init];
-    result.numerator = self.numerator * f.denominator;
-    result.denominator = self.denominator * f.numerator;
-    [result reduce];
-    
-    return result;
-}
-@end
+#import <Foundation/Foundation.h>
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        Fraction *a = [[Fraction alloc] init];
-        Fraction *b = [[Fraction alloc] init];
-        Fraction *result;
+        int Fibonacci[15], i;
         
-        [a setTo:1 over:3];
-        [b setTo:2 over:5];
+        Fibonacci[0] = 0; /* by definition */
+        Fibonacci[1] = 1; /* ditto */
         
-        [a print]; NSLog(@" +"); [b print]; NSLog(@"-----");
-        result = [a add:b];
-        [result print];
-        NSLog(@"\n");
+        for (i=2; i<15; ++i) {
+            Fibonacci[i] = Fibonacci[i-2] + Fibonacci[i-1];
+        }
         
-        [a print]; NSLog(@"  -"); [b print]; NSLog(@"-----");
-        result = [a sub: b];
-        [result print];
-        NSLog(@"\n");
-        
-        [a print]; NSLog(@"  *"); [b print]; NSLog(@"-----");
-        result = [a mul:b];
-        [result print];
-        NSLog(@"\n");
-        
-        [a print]; NSLog(@"  /"); [b print]; NSLog(@"-----");
-        result = [a div:b];
-        [result print];
-        NSLog(@"\n");
+        for (i=0; i<15; ++i) {
+            NSLog(@"%i", Fibonacci[i]);
+        }
     }
     return 0;
 }
