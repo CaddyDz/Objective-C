8dea3cb905d86b8e610577e6dd64a22fcd5fd6e9
Salim Djerbouh <github@caddydz.me>
2018-06-24 21:37:26 +0100

    Initializing arrays
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,21 +1,12 @@
-// Program to generate the first 15 Fibonacci numbers
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        int Fibonacci[15], i;
-        
-        Fibonacci[0] = 0; /* by definition */
-        Fibonacci[1] = 1; /* ditto */
-        
-        for (i=2; i<15; ++i) {
-            Fibonacci[i] = Fibonacci[i-2] + Fibonacci[i-1];
-        }
-        
-        for (i=0; i<15; ++i) {
-            NSLog(@"%i", Fibonacci[i]);
+        char word[] = { 'H', 'e', 'l', 'l', 'o', '!'};
+        int i;
+        for (i = 0; i < 6; ++i) {
+            NSLog(@"%c", word[i]);
         }
     }
     return 0;
