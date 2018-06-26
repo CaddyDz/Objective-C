dda71ed51c9aa9a8a1ca7d34b43f72b2b935e5a2
Salim Djerbouh <github@caddydz.me>
2018-06-26 16:03:55 +0100

    Block var readonly
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -8,9 +8,11 @@ int main(int argc, char * argv[])
         void (^printFoo)(void) =
             ^(void) {
                 NSLog(@"foo = %i", foo);
+                foo = 20; // ** THIS LINE GENERATES A COMPILER ERROR
             };
         foo = 15;
         printFoo();
+        NSLog(@"foo = %i", foo);
     }
     return 0;
 }
