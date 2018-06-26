fde607e3fcc214f1d277243d044fee931081e65c
Salim Djerbouh <github@caddydz.me>
2018-06-26 16:02:06 +0100

    Program 13.8 block var scope
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,22 +1,16 @@
 #import <Foundation/Foundation.h>
 
-// Block to calculate the nth triangular number
-
-void (^calculateTriangularNumber) (int) = ^(int n) {
-    int i, triangularNumber = 0;
-    
-    for (i = 1; i <= n; ++i)
-        triangularNumber += i;
-    
-    NSLog(@"Triangular number %i is %i", n, triangularNumber);
-};
-
-int main()
+int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        calculateTriangularNumber(10);
-        calculateTriangularNumber(20);
-        calculateTriangularNumber(50);
+        int foo = 10;
+        
+        void (^printFoo)(void) =
+            ^(void) {
+                NSLog(@"foo = %i", foo);
+            };
+        foo = 15;
+        printFoo();
     }
     return 0;
 }
