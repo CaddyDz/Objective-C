f4add11bb4fed73ebfb4dc3cf9786d7a97f8d0ce
Salim Djerbouh <github@caddydz.me>
2018-06-26 15:37:02 +0100

    Program 13.4 Function to calculat the nth triangular number
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,14 +1,22 @@
 #import <Foundation/Foundation.h>
 
-void printMessage(void)
+// Function to calculate the nth triangular number
+
+void calculateTriangularNumber(int n)
 {
-    NSLog(@"Programming is fun.");
+    int i, triangularNumber = 0;
+    for (i = 1; i <= n; ++i) {
+        triangularNumber += i;
+    }
+    NSLog(@"Triangular number %i is %i", n, triangularNumber);
 }
 
 int main(int arc, char * argv[])
 {
     @autoreleasepool {
-        printMessage();
+        calculateTriangularNumber(10);
+        calculateTriangularNumber(20);
+        calculateTriangularNumber(50);
     }
     return 0;
 }
