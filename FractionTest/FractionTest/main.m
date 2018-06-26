053d99649c9f922179b6f6ca0928efceaa0de62b
Salim Djerbouh <github@caddydz.me>
2018-06-26 15:32:42 +0100

    Program 13.3 calling a function in main
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,13 +1,14 @@
 #import <Foundation/Foundation.h>
 
-int main(int argc, char * argv[])
+void printMessage(void)
+{
+    NSLog(@"Programming is fun.");
+}
+
+int main(int arc, char * argv[])
 {
     @autoreleasepool {
-        char word[] = { 'H', 'e', 'l', 'l', 'o', '!'};
-        int i;
-        for (i = 0; i < 6; ++i) {
-            NSLog(@"%c", word[i]);
-        }
+        printMessage();
     }
     return 0;
 }
