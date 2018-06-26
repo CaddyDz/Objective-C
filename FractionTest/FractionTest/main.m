289502777a3f5bcd795b0da11c8adaff96bcfcb3
Salim Djerbouh <github@caddydz.me>
2018-06-26 15:56:41 +0100

    Block declaration and call
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,28 +1,12 @@
 #import <Foundation/Foundation.h>
 
-// This function finds the greatest common divisor of two nonnegative integer values and returns the result
-
-int gcd(int u, int v)
-{
-    int temp;
-    while (v != 0)
-    {
-        temp = u % v;
-        u = v;
-        v = temp;
-    }
-    return u;
-}
-
 int main()
 {
     @autoreleasepool {
-        int result;
-        result = gcd(150, 35);
-        NSLog(@"The gcd of 150 and 35 is %i", result);
-        result = gcd(1026, 405);
-        NSLog(@"The gcd of 1026 and 405 is %i", result);
-        NSLog(@"The gcd of 83 and 240 is %i", gcd(83, 240));
+        void (^printMessage) (void) = ^(void) {
+            NSLog(@"Programming is fun.");
+        };
+        printMessage();
     }
     return 0;
 }
