2309dc1dd44e42270828ef3f0d7a5b0cbf08e284
Salim Djerbouh <github@caddydz.me>
2018-06-28 11:35:13 +0100

    Program 13.14 pointers as arguments to functions
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,23 +1,27 @@
+// Pointers as arguments to functions
+
 #import <Foundation/Foundation.h>
+
+void exchange(int *pint1, int *pint2)
+{
+    int temp;
+    temp = *pint1;
+    *pint1 = *pint2;
+    *pint2 = temp;
+}
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        struct date
-        {
-            int month;
-            int day;
-            int year;
-        };
+        void exchange(int *pint1, int *pint2);
+        int i1 = -5, i2 = 66, *p1 = &i1, *p2 = &i2;
         
-        struct date today, *datePtr;
+        NSLog(@"i1 = %i, i2 = %i", i1, i2);
+        exchange(p1, p2);
+        NSLog(@"i1 = %i, i2 = %i", i1, i2);
         
-        datePtr = &today;
-        datePtr->month = 9;
-        datePtr->day = 25;
-        datePtr->year = 2014;
-        
-        NSLog(@"Today's date is %i/%i/%.2i.", datePtr->month, datePtr->day, datePtr->year % 100);
+        exchange(&i1, &i2);
+        NSLog(@"i1 = %i, i2 = %i", i1, i2);
     }
     return 0;
 }
