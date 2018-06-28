5f54d1e1a6e785f8d9438f17f268a6ff24d2e7b5
Salim Djerbouh <github@caddydz.me>
2018-06-28 08:54:33 +0100

    Program 13.11 to illustrate pointers
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- FractionTest/FractionTest/main.m
+++ FractionTest/FractionTest/main.m
@@ -1,22 +1,17 @@
+// Program to illustrate pointers
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        struct date
-        {
-            int month;
-            int day;
-            int year;
-        };
+        int count = 10, x;
+        int *intPtr;
         
-        struct date today;
+        intPtr = &count;
+        x = *intPtr;
         
-        today.month = 6;
-        today.day = 25;
-        today.year = 2018;
-        
-        NSLog(@"Today's date is %i/%i/%.2i.", today.day, today.month, today.year % 100);
+        NSLog(@"count = %i, x = %i", count, x);
         
     }
     return 0;
